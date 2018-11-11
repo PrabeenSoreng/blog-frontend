a7495444a729bdf84971e7cd21ca715d4d9dfeec
Prabeen <prabeensoreng2119@gmail.com>
2018-11-12 00:57:50 +0530

    implement view blog

--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog-view',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogViewComponent implements OnInit {
 
-  constructor() { }
+  public currentBlog;
+
+  public allBlogs = [
+    {
+      blogId: 1,
+      created: new Date(2018, 1, 20),
+      author: "Admin",
+      category: "Comedy",
+      isPublished: true,
+      views: 0,
+      description: "This is Blog 1 description",
+      title: "This is Blog 1",
+    },
+    {
+      blogId: 2,
+      created: new Date(2018, 2, 20),
+      author: "Admin",
+      category: "Comedy",
+      isPublished: true,
+      views: 23,
+      description: "This is Blog 2 description",
+      title: "This is Blog 2",
+    },
+    {
+      blogId: 3,
+      created: new Date(2018, 3, 20),
+      author: "Admin",
+      category: "Comedy",
+      isPublished: true,
+      views: 21,
+      description: "This is Blog 3 description",
+      title: "This is Blog 3",
+    }
+  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    let myBlogId = this.route.snapshot.paramMap.get('blogId');
+    this.singleBlog(myBlogId);
+  }
+
+  public singleBlog(currentBlogId): any{
+    for(let blog of this.allBlogs) {
+      if(blog.blogId == currentBlogId)
+        this.currentBlog = blog;
+    }
   }
 
 }
