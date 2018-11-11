6a4383b5e7aa736c143347b8a99b8a1071fec703
Prabeen <prabeensoreng2119@gmail.com>
2018-11-12 00:20:25 +0530

    implementing home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public allBlogs = [
+    {
+      blogId: 1,
+      created: new Date(2018, 1, 20),
+      author: "Admin",
+      category: "Comedy",
+      isPublished: true,
+      views: 0,
+      description: "This is Blog 1 description",
+      title: "This is Blog 1",
+    },
+    {
+      blogId: 2,
+      created: new Date(2018, 2, 20),
+      author: "Admin",
+      category: "Comedy",
+      isPublished: true,
+      views: 23,
+      description: "This is Blog 2 description",
+      title: "This is Blog 2",
+    },
+    {
+      blogId: 3,
+      created: new Date(2018, 3, 20),
+      author: "Admin",
+      category: "Comedy",
+      isPublished: true,
+      views: 21,
+      description: "This is Blog 3 description",
+      title: "This is Blog 3",
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
